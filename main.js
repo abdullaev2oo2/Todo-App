c1228476287c2491af7941984b6695f59fc8dbca
abdullaev2oo2 <76660654+abdullaev2oo2@users.noreply.github.com>
2022-01-09 15:29:37 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,23 +1,14 @@
+// variables
 const addBtn = document.getElementById('add');
 const finishedBtn = document.getElementById('finished');
 const currentBtn = document.getElementById('current');
 
-const notes = JSON.parse(localStorage.getItem('notes'));
-    if (notes) {
-        notes.forEach(note => {
-            addNewNote(note);
-        })
-    }
-    
-    addBtn.addEventListener('click', () => {
-        addNewNote();
-    });
-    
-    
-    let listFirst = document.querySelector('.current-list');
-    let listSecond = document.querySelector('.done-list');
+let listFirst = document.querySelector('.current-list');
+let listSecond = document.querySelector('.done-list');
 
-    currentBtn.classList.toggle('hidden');
+
+listSecond.classList.add('hidden');
+currentBtn.classList.toggle('hidden');
 
 currentBtn.addEventListener('click', () => {
     if (currentBtn.classList.contains('hidden')) {
@@ -53,67 +44,138 @@ finishedBtn.addEventListener('click', () => {
         listFirst.classList.add('hidden');
     }
 });
-    
-    function addNewNote(text = '') {
-        const note = document.createElement('div');
+
+const notes = JSON.parse(localStorage.getItem('notes'));
+
+if (notes) {
+    notes.forEach(note => {
+        if(note.includes('class=done')){
+            const noteValue = note.replace('class=done', '');
+            addToFinished(noteValue);
+        }else if(note.includes('hidden')){
+            const noteValue = note.replace('hidden', '');
+            addToFinished(noteValue);
+        }else {
+            addNewNote(note)
+        }
+    })
+};
+addBtn.addEventListener('click', () => {
+    addNewNote();
+});
+
+function addNewNote(text = '') {
+    const note = document.createElement('div');
+    note.classList.add('note');
+
+    note.innerHTML = `
+        <div class="notes">
+        <div class="tools">
+        <button class="done"><i class="fas fa-check"></i></button>
+        <button class="edit"><i class="fas fa-edit"></i></button>
+        <button class="delete"><i class="fas fa-trash"></i></button>
+        </div>
+        <div class="main ${text? '' : 'hidden'}"></div>
+        <textarea class="${text? 'hidden' : ''}"></textarea>
+        </div>
+        `;
+
+        const editBtn = note.querySelector('.edit');
+        const deleteBtn = note.querySelector('.delete');
+        const doneBtn = note.querySelector('.done');
+        
+        const main = note.querySelector('.main');
+        const textArea = note.querySelector('textarea');
+
+        let listFirst = document.querySelector('.current-list');
+        let listSecond = document.querySelector('.done-list');
+        
+        textArea.value = text;
+        main.innerHTML = marked(text);
+        
+        doneBtn.addEventListener('click', () => {
+            listSecond.appendChild(note);
+            doneBtn.classList.add('hidden');
+            editBtn.classList.add('hidden');
+            textArea.classList.add('class=done');
+        })
+        
+        editBtn.addEventListener('click', () => {
+            main.classList.toggle('hidden');
+            textArea.classList.toggle('hidden');
+        });
+        
+        deleteBtn.addEventListener('click', () => {
+            note.remove();
+            updateLS()
+        })
+        textArea.addEventListener('input', (e) => {
+            const { value } = e.target;
+            
+            main.innerHTML = marked(value);
+            
+            updateLS()
+        });
+        listFirst.appendChild(note);
+}
+
+function addToFinished(text = '') {
+    const note = document.createElement('div');
     note.classList.add('note');
     
     note.innerHTML = `
     <div class="notes">
     <div class="tools">
-    <button class="done"><i class="fas fa-check"></i></button>
-    <button class="edit"><i class="fas fa-edit"></i></button>
     <button class="delete"><i class="fas fa-trash"></i></button>
     </div>
     <div class="main ${text? '' : 'hidden'}"></div>
     <textarea class="${text? 'hidden' : ''}"></textarea>
     </div>
     `;
-    const editBtn = note.querySelector('.edit');
+
     const deleteBtn = note.querySelector('.delete');
-    const doneBtn = note.querySelector('.done');
-    
+
     const main = note.querySelector('.main');
     const textArea = note.querySelector('textarea');
 
-    let listFirst = document.querySelector('.current-list');
     let listSecond = document.querySelector('.done-list');
-    
+        
     textArea.value = text;
     main.innerHTML = marked(text);
-    
-    doneBtn.addEventListener('click', () => {
-        listSecond.appendChild(note);
-        doneBtn.classList.add('hidden');
-        editBtn.classList.add('hidden');
-    })
-    
-    editBtn.addEventListener('click', () => {
-        main.classList.toggle('hidden');
-        textArea.classList.toggle('hidden');
-    });
-    
+
     deleteBtn.addEventListener('click', () => {
         note.remove();
         updateLS()
     })
-    textArea.addEventListener('input', (e) => {
-        const { value } = e.target;
-        
-        main.innerHTML = marked(value);
-        
-        updateLS()
-    });
-    listFirst.appendChild(note);
+    listSecond.appendChild(note);
 }
+
 function updateLS() {
     const notesText = document.querySelectorAll('textarea');
-    
     const notes = [];
     
     notesText.forEach(note => {
-        notes.push(note.value);
+        notes.push(note.value + note.className);
     });
     
     localStorage.setItem('notes', JSON.stringify(notes));
 }
+
+const contactBtn = document.querySelector('.contact');
+const closeBtn = document.querySelector('.close');
+const panel = document.querySelector('.contact-me');
+
+contactBtn.addEventListener('click', () => {
+    if(contactBtn.classList.contains('hidden')) {
+        contactBtn.classList.remove('hidden')
+    }else {
+        panel.classList.add('opened')
+        contactBtn.classList.add('hidden')
+    }
+})
+
+closeBtn.addEventListener('click', () => {
+        panel.classList.remove('opened')
+        contactBtn.classList.remove('hidden')
+    }
+)
